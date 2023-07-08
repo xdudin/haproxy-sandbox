1c8579f5d372578de272fed14e42ab5e99c87a70
Alexey Dudin <xdudin@fi.muni.cz>
2023-07-08 12:47:59 +0200

    actuator

--- src/main/java/com/example/haproxy_sandbox/SandboxController.java
+++ src/main/java/com/example/haproxy_sandbox/SandboxController.java
@@ -1,11 +1,14 @@
 package com.example.haproxy_sandbox;
 
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Duration;
 
 @RestController
 @Slf4j
@@ -29,5 +32,12 @@ public class SandboxController {
 
     private String getFormattedLoad() {
         return String.format("Id = %s, load = %d", id, load);
+    }
+
+    @SneakyThrows
+    @GetMapping("/heavy")
+    public String heavyRequest() {
+        Thread.sleep(Duration.ofSeconds(5L));
+        return "OK";
     }
 }
